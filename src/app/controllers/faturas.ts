15beec410e17d787c8887ca3ba3b68bff5e9ea90
Buzzgrow <artur_pg2011@hotmail.com>
2020-11-17 13:35:34 -0300

    removing extension file from require

--- src/app/controllers/faturas.ts
+++ src/app/controllers/faturas.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 const mongoose = require('mongoose');
 
-const Fatura = require('../models/faturas.ts');
+const Fatura = require('../models/faturas');
 
 const isThereAnyBodyParamUndefined = require('../utils');
 
