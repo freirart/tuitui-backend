029cc8b2140f825f4725b1860138350d57f34af2
freirart <artur.santos@stone.com.br>
2022-11-12 21:43:32 -0300

    auth on tags routes, custom msg on expired token

--- src/app/auth/index.ts
+++ src/app/auth/index.ts
@@ -1,13 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-interface User {
-  id?: string;
-}
+import UserModel from "../models/user";
 
-const User = require("../models/user");
-
-const validateToken = (req: Request, res: Response, next: NextFunction) => {
+export const validateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     return res.status(401).json({ error: "No token provided." });
@@ -31,8 +31,13 @@ const validateToken = (req: Request, res: Response, next: NextFunction) => {
   jwt.verify(
     token,
     process.env.SECRET_KEY as jwt.Secret,
-    async (err, decoded?: User) => {
-      const user = await User.findById(decoded?.id).exec();
+    async (err, decoded?: { id: string }) => {
+      if (err instanceof jwt.TokenExpiredError) {
+        return res.status(401).json({ error: "Token expired" });
+      }
+
+      const user = await UserModel.findById(decoded?.id).exec();
+
       if (!user || err) {
         return res.status(401).json({ error: "Invalid token." });
       }
@@ -42,5 +47,3 @@ const validateToken = (req: Request, res: Response, next: NextFunction) => {
     }
   );
 };
-
-module.exports = validateToken;
